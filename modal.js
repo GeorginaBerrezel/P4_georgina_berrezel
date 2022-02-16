41c7d861e79c2902cf90863c6a98b9d4dfe23dd7
georgina berrezel <georgina.berrezel@gmail.com>
2022-02-16 17:32:03 +0100

    feat: close the modal and clean html

--- modal.js
+++ modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector("#closeform"); // fermer la modal en cliquand sur la croix
+const modalCloseValidation = document.querySelector("#closevalidation");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+/*---- Fonction qui permettent de fermer la modale ----*/
 
+// launch modal event
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click",() => modalbg.style.display = "block"));
+
+// Click to leave
+modalClose.addEventListener("click",() => modalbg.style.display = "none");
+
+// Click to leave modal Validation
+modalClose.addEventListener("click",() => modalCloseValidation.style.display = "none");
