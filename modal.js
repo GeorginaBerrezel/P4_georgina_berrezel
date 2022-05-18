2756bf5b9d291a3b21137fff1161ef7f57f0e101
Georgina <georgina.berrezel@gmail.com>
2022-05-18 22:34:07 +0200

    add email and birthday functions

--- modal.js
+++ modal.js
@@ -36,11 +36,8 @@ function controlInput() {
 // FORMULAIRE
 
 // Définition des variables
-
 let firstNameValid = false;
 let lastNameValid = false;
-/*console.log(lastNameValid);*/
-
 let emailValid = false;
 let birthdayDateValid = false;
 let quantityTournamentValid = false;
@@ -52,7 +49,6 @@ let regExpNumbers = /^[0-9]*$/;
 
 
 // Définition des constantes
-
 const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
 const email = document.querySelector("#email");
@@ -62,13 +58,8 @@ const wichTown = document.querySelectorAll('input[name="location"]');
 const conditionUser = document.querySelector("#checkbox1");
 
 // Définition des constantes d'erreur 
-
 const errorFirstName = document.querySelector("#errorfirstname");
-/*console.log(errorFirstName);*/
-
 const errorLastName = document.querySelector("#errorlastname");
-/*console.log(errorLasttName);*/
-
 const errorEmail = document.querySelector("#erroremail");
 const errorBirthdate = document.querySelector("#errorbirthdate");
 const errorQuantityTournament = document.querySelector("#errorquantitytournament");
@@ -81,41 +72,20 @@ function launchModal() {
 }
 
 // Fonction qui permettent de fermer la modale
-
+/*
 // launch modal event
 // modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+*/
 modalBtn.forEach((btn) => btn.addEventListener("click", () => modalbg.style.display = "block"));
 
 
 // Click to leave modal Validation
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Vérification des champs prénom, nom et email
 modalBtnSubmit.addEventListener("click", controlForm);
 function controlForm(event) {
   event.preventDefault();
-  console.log('controlForm')
   firstNameValid = controlInputNames(firstName, errorFirstName,
     "Veuillez entrer 2 caractères ou plus pour le champ du prénom");
 
@@ -124,15 +94,13 @@ function controlForm(event) {
 
   emailValid = controlInputEmail(email, errorEmail,
     "L'adresse email saisie est incorrecte.");
-/*
-  birthdayDateValid = birthdayDatecheck () {
-if 
+
+  birthdayDateValid = birthdayDatecheck (birthdayDate, errorBirthdate,
+    "Veuillez précisez votre date de naissance dans ce champ.");
   }
-*/
-}
 
 /*Nouvelle fonctions*/
-// Fonction de vérification des champs
+// Fonction de vérification des champs prénom et nom
 function controlInputNames(input, inputError, textErrorEmpty) {
 
   if (input.value.length < 2) {
@@ -146,24 +114,45 @@ function controlInputNames(input, inputError, textErrorEmpty) {
     inputError.innerHTML = "";
     return true;
   }
-
 }
 
-function controlInputEmail(input, inputError, textError) {
-console.log(input.value)
+// Fonction de vérification du champ email
+function controlInputEmail(input, inputError, textErrorEmpty) {
   const validEmail = regExpEmail.test(input.value)
-  console.log(validEmail)
-  return validEmail
+  /*return validEmail*/
+  if (input.value) {
+    inputError.innerHTML = textErrorEmpty;
+    input.style.border = "2px solid #e54858";
+    return false;
+  }
 
+  else {
+    input.style.border = "0px";
+    inputError.innerHTML = "";
+    return emailValid;
+    /*return validEmail;*/
+  }
 }
 
 /*function birthdayDatecheck*/
-function birthdayDatecheck(birthdayDate) {
+function birthdayDatecheck(birthdayDate, errorBirthdate) {
+
+  if (birthdayDate.value == "") {
+    errorBirthdate.innerHTML = "Veuillez précisez votre date de naissance dans ce champ.";
+    birthdayDate.style.border = "2px solid #e54858";
+    birthdayDate = false;
+  }
+  
+  else {
+    birthdayDateValid = true;
+    birthdayDate.style.border = "Opx";
+    errorBirthdate.innerHTML = "";
+  }
 
 }
-
+/*
 // Vérification du champ Date de naissance
-birthdayDate = controlInput(input, inputError, textErrorEmpty, textError, isValid) 
+birthdayDate = birthdayDatecheck(input, inputError, textErrorEmpty, textError, isValid) 
 
   if (birthdayDate.value == "") {
     errorBirthdate.innerHTML = "Veuillez précisez votre date de naissance dans ce champ.";
@@ -174,9 +163,9 @@ birthdayDate = controlInput(input, inputError, textErrorEmpty, textError, isVali
     birthdayDate.style.border = "Opx";
     errorBirthdate.innerHTML = "";
   }
+*/
 
   // Vérification du champ nombre de tournois GameOn
-
   quantityTournamentValid = controlInput(quantityTournament, errorQuantityTournament,
     "Veuillez préciser dans ce champ le nombre de tournois GameOn auquel vous avez participé.",
     "Vous devez saisir seulement des chiffres.",
